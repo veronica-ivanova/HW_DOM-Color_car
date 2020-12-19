e3f40d7ec66a0df786b05a76e75e8bc84cc1629f
veronica-ivanova <veronica.ivanova27@gmail.com>
2020-12-19 19:02:36 +0300

    2 вариант
    Доделан 2ой вариант через цикл и data  атрибут

--- js/main.js
+++ js/main.js
@@ -16,66 +16,66 @@ https://webcademy.ru/files/js2020/solaris/white.png
 https://webcademy.ru/files/js2020/solaris/white-pure.png
 */
 
-// Объявляем переменную цвета авто
-var imgColor = document.querySelector('img[alt="Автомобиль"]');
-console.log(imgColor)
+// // Объявляем переменную цвета авто
+// var imgColor = document.querySelector('img[alt="Автомобиль"]');
+// console.log(imgColor)
 
-// Прослушиваем клик по внешнему контейнеру
-colorsSelector.addEventListener('click', function(event){
+// // Прослушиваем клик по внешнему контейнеру
+// colorsSelector.addEventListener('click', function(event){
     
-    // Определяем именно тот элемент по которому кликнули
-    colorItem = event.target;
+//     // Определяем именно тот элемент по которому кликнули
+//     colorItem = event.target;
 
-    // Добавляем условие, чтобы исключить colorsSelector из выбора по клику - этот "костыль" не нравится, но пока не нашла способа обойти
-    if (colorItem.classList.contains("colorsSelector")){
-        return
-    } 
+//     // Добавляем условие, чтобы исключить colorsSelector из выбора по клику - этот "костыль" не нравится, но пока не нашла способа обойти
+//     if (colorItem.classList.contains("colorsSelector")){
+//         return
+//     } 
     
-    //Удаляем класс active у элемента у которого он был 
-    document.querySelector('.colorItem--active').classList.remove("colorItem--active");
+//     //Удаляем класс active у элемента у которого он был 
+//     document.querySelector('.colorItem--active').classList.remove("colorItem--active");
     
-    // Добавляем класс active элементу, по которому был клик
-    colorItem.classList.add("colorItem--active");
+//     // Добавляем класс active элементу, по которому был клик
+//     colorItem.classList.add("colorItem--active");
    
-    // Задаем соответствие цветов через условие
-    if (colorItem.classList.contains("colorBlue")) {
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/blue.png";
-    } else if (colorItem.classList.contains("colorRed")) {
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/red.png";
-    } else if (colorItem.classList.contains("colorWhite")){
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/white.png";
-    } else if (colorItem.classList.contains("colorWhitePure")) {
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/white-pure.png";
-    } else if (colorItem.classList.contains("colorWhite")) {
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/white.png";
-    } else if (colorItem.classList.contains("colorOrange")) {
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/orange.png";
-    } else if (colorItem.classList.contains("colorGraphite")){
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/graphite.png";
-    } else {
-        imgColor.src = "//webcademy.ru/files/js2020/solaris/black.png";
-    }
-})
-
+//     // Задаем соответствие цветов через условие
+//     if (colorItem.classList.contains("colorBlue")) {
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/blue.png";
+//     } else if (colorItem.classList.contains("colorRed")) {
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/red.png";
+//     } else if (colorItem.classList.contains("colorWhite")){
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/white.png";
+//     } else if (colorItem.classList.contains("colorWhitePure")) {
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/white-pure.png";
+//     } else if (colorItem.classList.contains("colorWhite")) {
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/white.png";
+//     } else if (colorItem.classList.contains("colorOrange")) {
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/orange.png";
+//     } else if (colorItem.classList.contains("colorGraphite")){
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/graphite.png";
+//     } else {
+//         imgColor.src = "//webcademy.ru/files/js2020/solaris/black.png";
+//     }
+// })
 
 
 // Получаем массив элементов - круги с цветом
-// var colorItems = document.querySelectorAll('.colorItem');
-// console.log(colorItems)
+var colorItems = document.querySelectorAll('.colorItem');
+// Получаем переменную переменную цвета авто (тэг img)
+var imgColor = document.querySelector('img[alt="Автомобиль"]');
+console.log(imgColor)
 
-
-// for (i = 0; i < colorItems.length; i++) {
-
-//     // Удаляем класс active у элемента у которого он был
-//     document.querySelector('.colorItem--active').classList.remove("colorItem--active");
-    
-//     // Прослушиваем клик по каждому элементу
-//     colorItems[i].addEventListener('click', function(){
-//         // Добавляем класс active
-//         // this.classList.add("colorItem--active");
-//         colorItems[i].classList.add("colorItem--active");
-//     });
-// }
+for (i = 0; i < colorItems.length; i++) {
+   
+    // Прослушиваем клик по каждому элементу
+    colorItems[i].addEventListener('click', function(){
+        // Удаляем класс active у элемента у которого он был
+        document.querySelector('.colorItem--active').classList.remove("colorItem--active");
+        // Добавляем класс active
+        this.classList.add("colorItem--active");
+        // Меняем общий путь к файлу, подставляя значение атрибута data-color
+        imgColor.src = `//webcademy.ru/files/js2020/solaris/${this.dataset.color}.png`;
+    });
+}
 
 
 
